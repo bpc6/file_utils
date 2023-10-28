f9841abef8a81c57930bd981ae7488193d652aa0
Bryan Covell <bpc6@vt.edu>
2023-10-28 11:04:35 -0400

    separate out command processing

--- main.cpp
+++ main.cpp
@@ -4,35 +4,47 @@
 #include "user_input.h"
 
 
+bool process_command(const std::string& cmd, const std::vector<std::string>& args);
+
+
 int main() {
-    while (true) {
+    bool cont = true;
+    while (cont) {
         std::string input = collect_input();
         std::string cmd;
         std::vector<std::string> args;
         parse_input(input, cmd, args);
-
-        if (cmd == "cp") {
-            if (args.size() == 2) {
-                copy(args[0], args[1]);
-            } else {
-                std::cout << "Expected exactly 2 args" << std::endl;
-            }
-        } else if (cmd == "mv") {
-            if (args.size() == 2) {
-                move(args[0], args[1]);
-            } else {
-                std::cout << "Expected exactly 2 args" << std::endl;
-            }
-        } else if (cmd == "rm") {
-            if (!args.empty()) {
-                remove_all(args);
-            } else {
-                std::cout << "Expected at least 1 arg" << std::endl;
-            }
-        } else if (cmd == "exit") {
-            return EXIT_SUCCESS;
-        } else {
-            std::cout << "Unknown command: " << cmd << std::endl;
-        }
+        cont = process_command(cmd, args);
     }
+    return EXIT_SUCCESS;
+}
+
+
+bool process_command(const std::string& cmd, const std::vector<std::string>& args) {
+    if (cmd == "exit") {
+        return false;
+    }
+
+    if (cmd == "cp") {
+        if (args.size() == 2) {
+            copy(args[0], args[1]);
+        } else {
+            std::cout << "Expected exactly 2 args" << std::endl;
+        }
+    } else if (cmd == "mv") {
+        if (args.size() == 2) {
+            move(args[0], args[1]);
+        } else {
+            std::cout << "Expected exactly 2 args" << std::endl;
+        }
+    } else if (cmd == "rm") {
+        if (!args.empty()) {
+            remove_all(args);
+        } else {
+            std::cout << "Expected at least 1 arg" << std::endl;
+        }
+    } else {
+        std::cout << "Unknown command: " << cmd << std::endl;
+    }
+    return true;
 }
